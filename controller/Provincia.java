329720e0d180176f7c9639f566e89084d8a3403d
Lautaro Lopez <lauta.lopez93@gmail.com>
2015-08-23 19:52:19 -0300

    Aca tene un commit puto

---
+++ controller/Provincia.java
@@ -0,0 +1,24 @@
+package controller;
+
+public class Provincia {
+	
+	private String nombre;
+
+	public Provincia() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Provincia(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	
+}
