9a4c8f7b6bc6db9cbff6bf2abd32e642024f726c
castrop <pcastro@unrn.edu.ar>
2015-08-23 22:25:08 -0300

    Model commit

--- model/Taxi.java
+++ model/Taxi.java
@@ -1,5 +1,39 @@
 package model;
 
 public class Taxi {
+	private String patente;
+	private String chofer;
+	private String licencia;
+	
+	public Taxi(String patente, String chofer, String licencia) {
+		super();
+		this.patente = patente;
+		this.chofer = chofer;
+		this.licencia = licencia;
+	}
 
+	public String getPatente() {
+		return patente;
+	}
+
+	public void setPatente(String patente) {
+		this.patente = patente;
+	}
+
+	public String getChofer() {
+		return chofer;
+	}
+
+	public void setChofer(String chofer) {
+		this.chofer = chofer;
+	}
+
+	public String getLicencia() {
+		return licencia;
+	}
+
+	public void setLicencia(String licencia) {
+		this.licencia = licencia;
+	}
+	
 }
--- model/Test.java
+++ model/Test.java
@@ -11,11 +11,13 @@ public class Test {
 		SimpleDateFormat formatoHora = new SimpleDateFormat("hh:mm:ss");
 		SimpleDateFormat formatoFecha = new SimpleDateFormat("dd-MM-yyyy"); 
 		
-		
-		
 		Usuario u = new Usuario("Lautaro","Lopez","12345679","llopez@domain.com","97654321");
 		
+		Notificacion n = new Notificacion("Llegando", 20);
+		
 		Pedido p = new Pedido(0.0, formatoFecha.format(actual), formatoHora.format(actual), Pedido.FormaDePago.EFECTIVO, u);
+		
+		Taxi t = new Taxi("abc 123","El rey","Licenciado");
 		
 		
 
