25d458218bbe83d59903c05cefcfe0bd03b1587b
castrop <pcastro@unrn.edu.ar>
2015-08-23 21:56:40 -0300

    User class commit

--- model/Usuario.java
+++ model/Usuario.java
@@ -1,5 +1,50 @@
 package model;
 
 public class Usuario {
-
+	private String nombre;
+	private String apellido;
+	private String dni;
+	private String mail;
+	private String telefono;
+	
+	public Usuario(String nombre, String apellido, String dni, String mail, String telefono) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+		this.mail = mail;
+		this.telefono = telefono;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public String getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
 }
