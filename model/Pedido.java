1073ce23e020a063c17fc6cb5abf5d58792a3728
castrop <pcastro@unrn.edu.ar>
2015-08-23 22:18:33 -0300

    Commit message

--- model/Pedido.java
+++ model/Pedido.java
@@ -6,12 +6,12 @@ public class Pedido {
 	enum FormaDePago{CREDITO, DEBITO, EFECTIVO};
 	
 	private double precio;
-	private Date fecha;
-	private Date hora;
+	private String fecha;
+	private String hora;
 	private FormaDePago pago;
 	private Usuario usuario;
 	
-	public Pedido(double precio, Date fecha, Date hora, FormaDePago pago, Usuario usuario) {
+	public Pedido(double precio, String fecha, String hora, FormaDePago pago, Usuario usuario) {
 		super();
 		this.precio = precio;
 		this.fecha = fecha;
@@ -28,19 +28,19 @@ public class Pedido {
 		this.precio = precio;
 	}
 
-	public Date getFecha() {
+	public String getFecha() {
 		return fecha;
 	}
 
-	public void setFecha(Date fecha) {
+	public void setFecha(String fecha) {
 		this.fecha = fecha;
 	}
 
-	public Date getHora() {
+	public String getHora() {
 		return hora;
 	}
 
-	public void setHora(Date hora) {
+	public void setHora(String hora) {
 		this.hora = hora;
 	}
 
