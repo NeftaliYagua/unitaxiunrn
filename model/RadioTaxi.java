cbc2971c0451d344df792d2b3e5d5b566e8d63e2
Lautaro Lopez <lauta.lopez93@gmail.com>
2015-08-26 13:06:17 -0300

    Agregada la logica de asignacion del taxi

--- model/RadioTaxi.java
+++ model/RadioTaxi.java
@@ -1,18 +1,45 @@
 package model;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 public class RadioTaxi {
-	List<Taxi> listaDeTaxis;
+	private List<Pedido> pedidosPendientes;
+	private List<Taxi> listaDeTaxis;
+	
 
 	public RadioTaxi(){
 		listaDeTaxis = new LinkedList<>();
+		pedidosPendientes = new ArrayList<>();
+	}
+	
+	public void addTaxi(Taxi t){
+		listaDeTaxis.add(t);
 	}
 
-	public Taxi asignarTaxi(){
-		return taxiLibre();
+	
+	public Pedido getPedidoPendiente() {
+		Pedido p = pedidosPendientes.get(0);
+		pedidosPendientes.remove(0);
+		return p;
+	}
+
+
+
+	public void addPedidoPendiente(Pedido pedido) {		
+		this.pedidosPendientes.add(pedido);
+	}
+
+
+
+	public void asignarTaxi(){
+		Taxi taxi = taxiLibre();
+		taxi.setEstado(Taxi.Estado.OCUPADO);
+		Pedido pedido = this.getPedidoPendiente();
+		pedido.setEstado(Pedido.Estado.EN_CURSO);
+		pedido.setTaxi(taxi);
 	}
 	
 	private Taxi taxiLibre(){
@@ -21,7 +48,7 @@ public class RadioTaxi {
 		while (it.hasNext() && it.next().getEstado() != Taxi.Estado.LIBRE){
 			
 		}
-		
+		  		
 		return it.next();
 	}
 }
