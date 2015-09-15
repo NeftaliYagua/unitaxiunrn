9ff109c99543258a9b1882f86f41e437c38e6378
castrop <pcastro@unrn.edu.ar>
2015-09-15 01:43:58 -0300

    Se cambiaron los nombres de los metodos

--- dao/DAO.java
+++ dao/DAO.java
@@ -3,9 +3,9 @@ package dao;
 import java.util.List;
 
 public interface DAO<E> {
-	public abstract boolean save(E object);
-	public abstract void delete(E object);
-	public abstract List<E> list();
-	public abstract E get(E object);
-	public abstract E search(E object);
+	public abstract boolean guardar(E object);
+	public abstract void borrar(E object);
+	public abstract List<E> listar();
+	public abstract E obtener(E object);
+	public abstract E buscar(E object);
 }
